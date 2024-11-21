ef0da513a33964111d7f1dc91cc7da7811155c4c
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-21 10:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 //your JS code here. If required.
-let url=window.location.href;
-alert('The length of the URL is:'+url.length);
+ document.addEventListener("DOMContentLoaded", function() {
+            let url = window.location.href; // Get the current URL
+            alert('The length of the URL is: ' + url.length); // Show the length in an alert
+        });
